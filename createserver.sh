025e27aa951e479838b72bed47d8742429aeb78e
asd <joshua.r.sal@gmail.com>
2019-10-15 04:36:38 -0700

    added more -y

--- createserver.sh
+++ createserver.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sudo apt-get update
-sudo apt-get autoremove
+sudo apt-get -y update
+sudo apt-get -y autoremove
 sudo apt-get -y install default-jre-headless
 sudo apt-get -y install default-jdk-headless
 sudo apt-get -y install maven
